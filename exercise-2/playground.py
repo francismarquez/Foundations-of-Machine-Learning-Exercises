767f77ffc5e58ca55da57dcc0d0e9727f484a9d5
Francis Marquez <francisryanmarquez@gmail.com>
2020-11-07 05:52:51 +0800

    update exercise-2

--- exercise-2/playground.py
+++ exercise-2/playground.py
@@ -1,6 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from utils import gaussian_distribution
 
-d = np.random.laplace(loc=15, scale=3, size=500)
-print(d.shape)
+np.random.seed(14)
+
+mu, sigma = 0, 0.1
+s = np.random.normal(mu, sigma, 1500)
+
+# count, bins, ignored = plt.hist(s, 30, density=True)
+# plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+#                np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+#          linewidth=2, color='r')
+# plt.show()
+# exit()
+
+s1 = np.array(list(filter(lambda x: x <= (2*sigma) and x >= (-2*sigma), s)))
+
+dataset_len = 1000
+TRAIN_SPLIT = int(0.9 * dataset_len)
+TEST_SPLIT = int(0.1 * dataset_len)
+
+dataset_x = np.random.choice(s1, dataset_len)
+dataset_y = gaussian_distribution(dataset_x,sigma,mu)
+
+train_x, test_x = np.split(dataset_x, [TRAIN_SPLIT])
+train_y, test_y = np.split(dataset_y, [TRAIN_SPLIT])
+
+plt.plot(train_x, train_y, '.')
+plt.show()
+
+# initialize_parameters()
--- exercise-2/utils.py
+++ exercise-2/utils.py
@@ -1,17 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# activation functions
+def gaussian_distribution(x,sigma,mu):
+    return 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (x - mu)**2 / (2 * sigma**2) )
+
+# Sigmoid activation function
 def sigmoid(x):
     return np.power(1+np.exp(-x),-1)
 
+# ReLU activation function
 def relu(x):
     return max(0.0,x)
 
-def slice_dataset(data, min_val, max_val):
-    for i, val in enumerate(data):
-        # print(i)
-        # print(val)
-        if (val <= min_val) :
-            if i is None: print("wala") 
-            else: print(i, val)
+# mean-square error loss function
+def mse(observed, predicted):
+    y = np.subtract(observed-predicted)
+    y = np.square(y)
+    return np.mean(y)
+
+def initialize_weights():
+    mu, sigma = 0, 1 
+    w = np.random.normal(mu,sigma,1000)
+    b = 0
+    return w, b
+
+def model(train_x, train_y, test_x, train):
+    return
+
+def train(x,y,batch_size,learning_rate):
+    return history
+
+def evaluate(x,y,):
+    return loss, accuracy
